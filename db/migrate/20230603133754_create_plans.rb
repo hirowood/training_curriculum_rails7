3fe5983710699e703efe12bce634ac3119b22eaf
hirowood <hirowoodandmika730@gmail.com>
2025-03-15 15:17:53 +0900

    Isuue4~7修正

--- db/migrate/20230603133754_create_plans.rb
+++ db/migrate/20230603133754_create_plans.rb
@@ -1,8 +1,8 @@
 class CreatePlans < ActiveRecord::Migration[7.0]
   def change
     create_table :plans do |t|
-      t.string :plan
-      t.date :date
+      t.string :plan, null: false
+      t.date :date, null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_03_133754) do
-  create_table "plans", charset: "utf8mb4", force: :cascade do |t|
-    t.string "plan"
-    t.date "date"
+ActiveRecord::Schema[7.1].define(version: 2023_06_03_133754) do
+  create_table "plans", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "plan", null: false
+    t.date "date", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
